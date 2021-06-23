1bb259f6d859d7d04bd581cf485c2636659127c2
Jigesh Raval <raval.jigesh@gmail.com>
2021-06-23 11:55:27 -0600

    init

---
+++ server.js
@@ -0,0 +1,40 @@
+let app = require('express'); 
+let server = require('http').createServer(app);
+let io = require('socket.io')(server, {
+    origins: ["http://localhost:8101", 'http://localhost:8100'],
+    cors: {
+        origin: "http://localhost:8100",
+        methods: ["GET", "POST"]
+    }
+})
+let port = 3001;
+
+server.listen(port, () => {
+    console.log('listening on ' + port);
+})
+
+
+io.on('connection', (socket) => { 
+    
+    const user_id = +socket.handshake.query.id;
+
+    if (user_id) {
+        
+        socket.join(user_id);
+        socket.join(JSON.parse(socket.handshake.query.chats));
+        socket.emit('connection', user_id);
+
+    }
+    
+    socket.on('send-message', ({ id, message }) => {
+        socket.broadcast.to(+id).emit('receive-message', message);
+    })
+    
+    socket.on('set-typing', ({ id, name, typing }) => {
+        socket.broadcast.to(+id).emit('receive-typing', { name, typing });
+    })
+
+    socket.on('read-by', ({ id, read_by }) => {
+        socket.broadcast.to(+id).emit('receive-read-by', { id, read_by });
+    })
+});
